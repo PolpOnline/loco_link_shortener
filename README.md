ed6c26d36daa5d38177524b52b7fad6d87e7fe52
PolpOnline <git@t0mmy.anonaddy.com>
2024-05-09 20:52:04 +0200

    Format code

--- README.md
+++ README.md
@@ -4,18 +4,19 @@ Loco is a web and API framework running on Rust.
 
 This is the **SaaS starter** which includes a `User` model and authentication based on JWT.
 
-
 ## Quick Start
 
 You need:
 
-* A local postgres instance
-* A local Redis instance
+- A local postgres instance
+- A local Redis instance
 
 Check out your development [configuration](config/development.yaml).
 
-> To configure a database , please run a local postgres database with <code>loco:loco</code> and a db named <code>[app name]_development.</code>: 
-<code>docker run -d -p 5432:5432 -e POSTGRES_USER=loco -e POSTGRES_DB=[app name]_development -e POSTGRES_PASSWORD="loco" postgres:15.3-alpine</code>
+> To configure a database , please run a local postgres database with <code>loco:loco</code> and a db
+> named <code>[app name]\_development.</code>:
+> <code>docker run -d -p 5432:5432 -e POSTGRES_USER=loco -e POSTGRES_DB=[app name]\_development -e POSTGRES_PASSWORD="
+> loco" postgres:15.3-alpine</code>
 
 Now start your app:
 
--- frontend/src/lib/LinkCard.svelte
+++ frontend/src/lib/LinkCard.svelte
@@ -1,28 +1,30 @@
 <script lang="ts">
-    import LinkBrief from './LinkBrief.svelte';
-    import DefaultLinkImage from "./DefaultLinkImage.svelte";
+	import LinkBrief from './LinkBrief.svelte';
+	import DefaultLinkImage from './DefaultLinkImage.svelte';
 
-    export let name: string;
-    export let image: string;
-    export let original: string;
-    export let shortened: string;
-    export let createdAt: Date;
+	export let name: string;
+	export let image: string;
+	export let original: string;
+	export let shortened: string;
+	export let createdAt: Date;
 
-    let detailsPage = `details/${shortened}`;
-    let imageAlt = `Image for ${name}`;
+	let detailsPage = `details/${shortened}`;
+	let imageAlt = `Image for ${name}`;
 </script>
 
 <div class="card">
-    {#if image}
-        <img alt={imageAlt} class="card-img-top" src={image}/>
-    {:else}
-        <DefaultLinkImage class="w-100"/>
-    {/if}
-    <div class="card-body d-flex flex-column align-items-baseline">
-        <h5 class="card-title">{name}</h5>
-        <p class="card-text">
-            <LinkBrief createdAt={createdAt} original={original}/>
-        </p>
-        <a class="btn btn-primary mt-auto w-100" href={detailsPage}>Info</a>
-    </div>
+	{#if image}
+		<img alt={imageAlt} class="card-img-top" src={image} />
+	{:else}
+		<div class="w-100">
+			<DefaultLinkImage />
+		</div>
+	{/if}
+	<div class="card-body d-flex flex-column align-items-baseline">
+		<h5 class="card-title">{name}</h5>
+		<p class="card-text">
+			<LinkBrief createdAt={createdAt} original={original} />
+		</p>
+		<a class="btn btn-primary mt-auto w-100" href={detailsPage}>Info</a>
+	</div>
 </div>
