a860015721fb26615324224516bf42ee45c97770
Naiker <yinho999@gmail.com>
2024-05-15 16:13:10 +0100

    fixing session cookie

--- src/backend/src/initializers/axum_session.rs
+++ src/backend/src/initializers/axum_session.rs
@@ -1,5 +1,6 @@
 use async_trait::async_trait;
 use axum::Router as AxumRouter;
+use axum_extra::extract::cookie::SameSite;
 use loco_rs::prelude::*;
 
 pub struct AxumSessionInitializer;
@@ -12,8 +13,13 @@ impl Initializer for AxumSessionInitializer {
 
     async fn after_routes(&self, router: AxumRouter, _ctx: &AppContext) -> Result<AxumRouter> {
         // Create the session store configuration
-        let session_config =
-            axum_session::SessionConfig::default().with_table_name("sessions_table");
+        let session_config = axum_session::SessionConfig::default()
+            .with_table_name("sessions_table")
+            .with_http_only(false)
+            .with_secure(false)
+            .with_cookie_same_site(SameSite::None)
+            .with_cookie_domain("localhost:5173")
+            .with_cookie_domain("/login");
         // Create the session store
         let session_store =
             axum_session::SessionStore::<axum_session::SessionNullPool>::new(None, session_config)
--- src/frontend/src/routes/login/+page.svelte
+++ src/frontend/src/routes/login/+page.svelte
@@ -1,30 +1,30 @@
 <script lang="ts">
-	import FlatColorIconsGoogle from '~icons/flat-color-icons/google';
-	import { get } from '$lib/api';
+    import FlatColorIconsGoogle from '~icons/flat-color-icons/google';
+    import {get} from '$lib/api';
 
-	// let href = '';
-	// $ : {
-	// 	Promise.resolve(get('oauth2/google')
-	// 		.then(res => res.text())
-	// 		.then((x) => href = x)
-	// 		.catch(err => console.error(err)));
-	// }
-	async function redirect() {
-		const res = await get('oauth2/google');
+    // let href = '';
+    // $ : {
+    // 	Promise.resolve(get('oauth2/google')
+    // 		.then(res => res.text())
+    // 		.then((x) => href = x)
+    // 		.catch(err => console.error(err)));
+    // }
+    async function redirect() {
+        const res = await fetch(`http://localhost:3000/api/oauth2/google`, {});
+		// get the response header
+		console.log(res.headers.getSetCookie())
 
-		console.log(res.text());
+        if (!res.ok) {
+            throw Error(res.text());
+        }
 
-		if (!res.ok) {
-			throw Error(res.text());
-		}
-
-		window.location.href = await res.text();
-	}
+        // window.location.href = await res.text();
+    }
 </script>
 
 <div class="d-flex flex-column justify-content-center align-items-center vh-100">
-	<button class="btn btn-primary d-flex align-items-center" on:click={redirect}>
-		<FlatColorIconsGoogle />
-		<span class="ms-2">Login with Google</span>
-	</button>
+    <button class="btn btn-primary d-flex align-items-center" on:click={redirect}>
+        <FlatColorIconsGoogle/>
+        <span class="ms-2">Login with Google</span>
+    </button>
 </div>
