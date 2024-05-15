c1d8b54461c52e8611d24453556119f7acd02450
PolpOnline <git@t0mmy.anonaddy.com>
2024-05-15 23:28:13 +0200

    Start auth callback

--- src/backend/src/controllers/oauth2/protected.rs
+++ src/backend/src/controllers/oauth2/protected.rs
@@ -1,8 +1,4 @@
-use axum::{
-    debug_handler,
-    extract::State,
-    response::{IntoResponse, Response},
-};
+use axum::{debug_handler, extract::State, response::IntoResponse};
 use loco_oauth2::controllers::middleware::OAuth2CookieUser;
 use loco_rs::{
     app::AppContext,
