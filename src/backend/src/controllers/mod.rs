cc5294192fbcf1774687aebba24861e49c3e178a
PolpOnline <git@t0mmy.anonaddy.com>
2024-05-20 00:45:37 +0200

    Add System Info

--- src/backend/src/controllers/mod.rs
+++ src/backend/src/controllers/mod.rs
@@ -6,7 +6,10 @@ pub mod oauth2;
 pub mod retrieve;
 pub mod utils;
 pub mod validations;
+pub mod system_info;
 
 pub fn routes() -> Routes {
-    Routes::new().add("/x/:shortened", get(retrieve::retrieve))
+    Routes::new()
+        .add("/x/:shortened", get(retrieve::retrieve))
+        .add("/sys_info", get(system_info::sys_info))
 }
