dadc77f7254bfd9daf412183e10244da7c5b857b
PolpOnline <git@t0mmy.anonaddy.com>
2024-05-19 03:11:25 +0200

    Lightened color

--- src/frontend/src/lib/components/UnderlinedText.svelte
+++ src/frontend/src/lib/components/UnderlinedText.svelte
@@ -1,8 +1,14 @@
 <script lang="ts">
+	import tinycolor from 'tinycolor2';
+
 	export let color: string;
+
+	const lighteningLevel = 20;
+
+	const lightenedColor = tinycolor(color).lighten(lighteningLevel).toString();
 </script>
 
-<span class="custom-underline" style={`text-decoration-color: ${color};`}>
+<span class="custom-underline fw-bold" style={`text-decoration-color: ${color}; --lightened-color: ${lightenedColor}`}>
 	<slot />
 </span>
 
@@ -10,5 +16,9 @@
     .custom-underline {
         text-decoration: underline;
         text-underline-offset: 2px;
+    }
+
+    .custom-underline {
+        color: var(--lightened-color);
     }
 </style>
