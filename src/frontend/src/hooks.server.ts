aa6efb59bdbf59baa870745fcca92deaf141ea89
PolpOnline <git@t0mmy.anonaddy.com>
2024-11-07 00:46:03 +0100

    Forward cookies back and forth from the API

--- src/frontend/src/hooks.server.ts
+++ src/frontend/src/hooks.server.ts
@@ -1,7 +1,11 @@
-import type { HandleFetch, ResolveOptions, Handle } from '@sveltejs/kit';
+import type { Handle, HandleFetch, ResolveOptions } from '@sveltejs/kit';
 import { API_URL } from '$lib/api';
 import cookie from 'cookie';
 import type { LoginStatus } from './app';
+import { default as setCookieParser } from 'set-cookie-parser';
+
+// Cookie max age in seconds (400 days)
+export const COOKIE_ABSOLUTE_MAX_AGE = 34560000;
 
 // Forwards all cookies to the API, see https://kit.svelte.dev/docs/hooks#server-hooks-handlefetch
 export const handleFetch: HandleFetch = async ({ event, request, fetch }) => {
@@ -12,18 +16,58 @@ export const handleFetch: HandleFetch = async ({ event, request, fetch }) => {
 	}
 
 	// Check if client has the JWT cookie, and if so, forward it to the API
-	const cookies = event.request.headers.get('cookie');
-	if (!cookies) {
-		return fetch(request);
+	const cookies = event.request.headers.get('cookie') || undefined;
+	const parsedCookies = cookies ? cookie.parse(cookies) : undefined;
+
+	// If the client has a JWT cookie, forward it to the API in the Authorization header
+	if (parsedCookies) {
+		const jwtCookie = parsedCookies.jwt;
+
+		if (jwtCookie) {
+			request.headers.set('Authorization', `Bearer ${jwtCookie}`);
+		}
+
+		// Forward all other cookies to the API
+		const mergedCookies = Object.entries(parsedCookies)
+			// Filter the jwt cookie as we've already handled it
+			.filter(([name]) => name !== 'jwt')
+			// Filter out cookies with undefined values
+			.filter((pair): pair is [string, string] => pair[1] !== undefined)
+			.map(([name, value]) => cookie.serialize(name, value))
+			.join('; ');
+
+		request.headers.set('Cookie', mergedCookies);
 	}
 
-	const jwtCookie = cookie.parse(cookies).jwt;
+	const res = await fetch(request);
 
-	if (jwtCookie) {
-		request.headers.set('Authorization', `Bearer ${jwtCookie}`);
+	// If the API wants to set any cookies, forward them to the client
+	const setCookieHeader = res.headers.getSetCookie();
+
+	if (!setCookieHeader) {
+		return res;
 	}
 
-	return fetch(request);
+	const setCookies = setCookieParser.parse(setCookieHeader);
+
+	// Response did not contain a set-cookie header
+	if (!setCookies) {
+		return res;
+	}
+
+	// Forward the set-cookie header to the client
+	setCookies.forEach((cookie) => {
+		event.cookies.set(cookie.name, cookie.value, {
+			// @ts-ignore
+			sameSite: cookie.sameSite || 'strict',
+			path: cookie.path || '/',
+			maxAge: cookie.maxAge || COOKIE_ABSOLUTE_MAX_AGE,
+			httpOnly: true,
+			secure: true
+		});
+	});
+
+	return res;
 };
 
 // noinspection JSUnusedGlobalSymbols
