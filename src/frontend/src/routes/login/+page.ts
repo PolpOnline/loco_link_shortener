46283aeec98a9dc2d4db93a50fa99e6eb0cd279e
PolpOnline <git@t0mmy.anonaddy.com>
2024-11-07 01:17:59 +0100

    Do not ssr login

--- src/frontend/src/routes/login/+page.ts
+++ src/frontend/src/routes/login/+page.ts
@@ -1,7 +1,9 @@
 import { API_URL } from '$lib/api';
-import type { PageServerLoad } from '../$types';
+import type { PageLoad } from './$types';
 
-export const load: PageServerLoad = async ({ fetch }) => {
+export const ssr = false;
+
+export const load: PageLoad = async ({ fetch }) => {
 	const gotoUrl = await fetch(`${API_URL}/oauth2/google`, {
 		method: 'GET'
 	}).then((res) => res.text());
